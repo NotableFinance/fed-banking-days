4b0c9b06eaedf17a95230494eff3b971aa3f0dbb
Alec Perkins <mail@alecperkins.me>
2022-08-12 14:44:18 -0400

    Copy over nextBankingDay function, tests

---
+++ __tests__/nextBankingDay.test.ts
@@ -0,0 +1,226 @@
+
+import nextBankingDay, { checkIfFedBankHoliday, getDSTStart, getDSTEnd } from '../lib/nextBankingDay';
+
+const RECENT_DSTS: Array<[ number, [number,number], [number, number] ]> = [
+  [2014, [3, 9], [11, 2]],
+  [2015, [3, 8], [11, 1]],
+  [2016, [3, 13], [11, 6]],
+  [2017, [3, 12], [11, 5]],
+  [2018, [3, 11], [11, 4]],
+  [2019, [3, 10], [11, 3]],
+  [2020, [3, 8], [11, 1]],
+  [2021, [3, 14], [11, 7]],
+  [2022, [3, 13], [11, 6]],
+  [2023, [3, 12], [11, 5]],
+  [2024, [3, 10], [11, 3]],
+  [2025, [3, 9], [11, 2]],
+  [2026, [3, 8], [11, 1]],
+];
+
+describe('getDSTStart', function () {
+  it('should calculate the correct dates for recent DSTs', function () {
+    for (let def of RECENT_DSTS) {
+      const [year, start] = def;
+      const d = getDSTStart(year);
+      expect(d.getUTCFullYear()).toEqual(year);
+      expect(d.getUTCMonth()).toEqual(start[0] - 1);
+      expect(d.getUTCDate()).toEqual(start[1]);
+      expect(d.getUTCHours()).toEqual(6); // 2am Eastern
+    }
+  });
+});
+
+describe('getDSTEnd', function () {
+  it('should calculate the correct dates for recent DSTs', function () {
+    for (let def of RECENT_DSTS) {
+      const [year, , end] = def;
+      const d = getDSTEnd(year);
+      expect(d.getUTCFullYear()).toEqual(year);
+      expect(d.getUTCMonth()).toEqual(end[0] - 1);
+      expect(d.getUTCDate()).toEqual(end[1]);
+      expect(d.getUTCHours()).toEqual(7); // 2am Eastern
+    }
+  });
+});
+
+
+const RECENT_FED_HOLIDAYS = [
+  '2019-01-01',
+  '2019-01-21',
+  '2019-02-18',
+  '2019-05-27',
+  '2019-07-04',
+  '2019-09-02',
+  '2019-10-14',
+  '2019-11-11',
+  '2019-11-28',
+  '2019-12-25',
+  '2020-01-01',
+  '2020-01-20',
+  '2020-02-17',
+  '2020-05-25',
+  '2020-07-04', // Different from holiday for Federal employees (7/3)
+  '2020-09-07',
+  '2020-10-12',
+  '2020-11-11',
+  '2020-11-26',
+  '2020-12-25',
+  '2021-01-01',
+  '2021-01-18',
+  '2021-02-15',
+  '2021-05-31',
+  '2021-07-05', // 7/4 is Sunday
+  '2021-09-06',
+  '2021-10-11',
+  '2021-11-11',
+  '2021-11-25',
+  '2021-12-25',
+  '2022-01-01',
+  '2022-01-17',
+  '2022-02-21',
+  '2022-05-30',
+  '2022-06-20', // 6/19 is Sunday
+  '2022-07-04',
+];
+
+
+describe('checkIfFedBankHoliday', function () {
+  it('should match recent holidays', function () {
+    for (let def of RECENT_FED_HOLIDAYS) {
+      const d = new Date(`${ def }T12:00:00Z`);
+      const result = checkIfFedBankHoliday(d);
+      if (!result) { // Log it out to know which one failed
+        process.stdout.write(JSON.stringify({ def, d, result }) + '\n');
+      }
+      expect(result).toBeTruthy();
+    }
+  });
+});
+
+
+
+
+describe('nextBankingDay', function () {
+
+  it('should give monday for a sunday', function () {
+    const [date, holiday] = nextBankingDay(new Date('2020-02-23T12:00:00.000Z'));
+    expect(date.toISOString()).toEqual('2020-02-24T14:00:00.000Z'); // Note the time changes to 9am EST
+    expect(holiday).toBeUndefined();
+  });
+
+  it('should give tuesday for a monday', function () {
+    const [date, holiday] = nextBankingDay(new Date('2020-02-24T12:00:00.000Z'));
+    expect(date.toISOString()).toEqual('2020-02-25T14:00:00.000Z');
+    expect(holiday).toBeUndefined();
+  });
+
+  it('should give wednesday for a tuesday', function () {
+    const [date, holiday] = nextBankingDay(new Date('2020-02-25T12:00:00.000Z'));
+    expect(date.toISOString()).toEqual('2020-02-26T14:00:00.000Z');
+    expect(holiday).toBeUndefined();
+  });
+
+  it('should give thursday for a wednesday', function () {
+    const [date, holiday] = nextBankingDay(new Date('2020-02-26T12:00:00.000Z'));
+    expect(date.toISOString()).toEqual('2020-02-27T14:00:00.000Z');
+    expect(holiday).toBeUndefined();
+  });
+
+  it('should give friday for a thursday', function () {
+    const [date, holiday] = nextBankingDay(new Date('2020-02-27T12:00:00.000Z'));
+    expect(date.toISOString()).toEqual('2020-02-28T14:00:00.000Z');
+    expect(holiday).toBeUndefined();
+  });
+
+  it('should give monday for a friday', function () {
+    const [date, holiday] = nextBankingDay(new Date('2020-02-28T12:00:00.000Z'));
+    expect(date.toISOString()).toEqual('2020-03-02T14:00:00.000Z');
+    expect(holiday).toBeUndefined();
+  });
+
+  it('should give monday for a saturday', function () {
+    const [date, holiday] = nextBankingDay(new Date('2020-02-29T12:00:00.000Z'));
+    expect(date.toISOString()).toEqual('2020-03-02T14:00:00.000Z');
+    expect(holiday).toBeUndefined();
+  });
+
+  it('should give monday 9am EDT for the friday before DST start', function () {
+    const [date1, holiday1] = nextBankingDay(new Date('2020-03-07T09:00:00-0500'));
+    expect(date1.toISOString()).toEqual('2020-03-09T13:00:00.000Z');
+    expect(holiday1).toBeUndefined();
+  });
+
+  it('should give monday 9am EST for the friday before DST end', function () {
+    const [date1, holiday1] = nextBankingDay(new Date('2020-10-30T09:00:00-0400'));
+    expect(date1.toISOString()).toEqual('2020-11-02T14:00:00.000Z');
+    expect(holiday1).toBeUndefined();
+  });
+
+  it('should give wednesday for a monday after 5pm eastern (non-DST)', function () {
+    const [date1, holiday1] = nextBankingDay(new Date('2020-02-24T22:10:00.000Z'));
+    expect(date1.toISOString()).toEqual('2020-02-26T14:00:00.000Z');
+    expect(holiday1).toBeUndefined();
+    // Checking West Coast US time just for Nathan :P
+    const [date2, holiday2] = nextBankingDay(new Date('2020-02-24T15:10:00-0700'));
+    expect(date2.toISOString()).toEqual('2020-02-26T14:00:00.000Z');
+    expect(holiday2).toBeUndefined();
+  });
+
+  it('should give wednesday for a monday after 5pm eastern (DST)', function () {
+    const [date, holiday] = nextBankingDay(new Date('2020-06-01T21:10:00.000Z'));
+    expect(date.toISOString()).toEqual('2020-06-03T13:00:00.000Z');
+    expect(holiday).toBeUndefined();
+  });
+
+  it('should give thursday for a monday when count is 3', function () {
+    const [date, holiday] = nextBankingDay(new Date('2020-02-24T12:00:00.000Z'), 3);
+    expect(date.toISOString()).toEqual('2020-02-27T14:00:00.000Z');
+    expect(holiday).toBeUndefined();
+  });
+
+  it('should give tuesday for a thursday when count is 3', function () {
+    const [date, holiday] = nextBankingDay(new Date('2020-02-20T12:00:00.000Z'), 3);
+    expect(date.toISOString()).toEqual('2020-02-25T14:00:00.000Z');
+    expect(holiday).toBeUndefined();
+  });
+
+  it('should skip dynamic holidays relative to the start of the month', function () {
+    const [date, holiday] = nextBankingDay(new Date('2020-02-14T12:00:00.000Z'));
+    expect(date.toISOString()).toEqual('2020-02-18T14:00:00.000Z');
+    expect(holiday).toEqual("Washington's Birthday/Presidents' Day");
+  });
+
+  it('should skip dynamic holidays relative to the end of the month', function () {
+    const [date, holiday] = nextBankingDay(new Date('2020-05-22T12:00:00.000Z'));
+    expect(date.toISOString()).toEqual('2020-05-26T13:00:00.000Z'); // Note the time change to 9am EDT
+    expect(holiday).toEqual('Memorial Day');
+  });
+
+  it('should skip static holidays during week', function () {
+    const [date, holiday] = nextBankingDay(new Date('2019-12-31T12:00:00.000Z'));
+    expect(date.toISOString()).toEqual('2020-01-02T14:00:00.000Z');
+    expect(holiday).toEqual("New Year's Day");
+  });
+
+  it('should ignore static holidays on saturday', function () {
+    // Check the previous year to confirm this holiday is actually configured
+    const [date1, holiday1] = nextBankingDay(new Date('2019-07-03T12:00:00.000Z'));
+    expect(date1.toISOString()).toEqual('2019-07-05T13:00:00.000Z');
+    expect(holiday1).toEqual('Independence Day');
+    // On a Saturday in 2020 because it's a leap year
+    const [date2, holiday2] = nextBankingDay(new Date('2020-07-03T12:00:00.000Z'));
+    expect(date2.toISOString()).toEqual('2020-07-06T13:00:00.000Z');
+    expect(holiday2).toBeUndefined(); // Crosses Independence Day but should not count
+    // Confirm the Friday is not an observed holiday
+    const [date3, holiday3] = nextBankingDay(new Date('2020-07-02T12:00:00.000Z'));
+    expect(date3.toISOString()).toEqual('2020-07-03T13:00:00.000Z');
+    expect(holiday3).toBeUndefined();
+  });
+
+  it('should skip static holidays on sunday observed monday', function () {
+    const [date, holiday] = nextBankingDay(new Date('2021-07-03T12:00:00.000Z'));
+    expect(date.toISOString()).toEqual('2021-07-06T13:00:00.000Z');
+    expect(holiday).toEqual('Independence Day');
+  });
+
+});
---
+++ src/nextBankingDay.ts
@@ -0,0 +1,256 @@
+/*
+Federal Reserve Bank Holidays:
+
+https://www.federalreserve.gov/aboutthefed/k8.htm
+
+> For holidays falling on Saturday, Federal Reserve Banks and Branches will be
+> open the preceding Friday; however, the Board of Governors will be closed.
+> For holidays falling on Sunday, all Federal Reserve offices will be closed the
+> following Monday.
+
+Packages like @date/holiday unfortunately assume holidays on Saturday will be
+observed on the preceding Friday, as is standard procedure for Federal employees
+(https://www.opm.gov/policy-data-oversight/pay-leave/federal-holidays/), so we
+need a different implementation.
+
+nextBankingDay is anchored to US Eastern (America/New_York) timezone, and the
+result will always be start of business in that timezone (9am).
+
+*/
+
+import memoize = require('lodash/memoize');
+
+const ONE_HOUR_MS = 1000 * 60 * 60;
+const ONE_DAY_MS = ONE_HOUR_MS * 24;
+
+/* Holiday definition */
+
+type Def = number | [number, number];
+type Holidays = { [key: string]: [number, Def] }
+
+// Can be either exact month–date pair ([month,date]) or some Nth day of week
+// of month ([month,[0-indexed-day-of-week, n]]). Months 1-indexed to match reality.
+const HOLIDAYS: Holidays = {
+  "New Year's Day": [1,1],
+  "Birthday of Martin Luther King Jr.": [1,[1,3]], // 3rd Monday in January
+  "Washington's Birthday/Presidents' Day": [2,[1,3]], // 3rd Monday in February
+  "Memorial Day": [5,[1,-1]], // Last Monday in May
+  "Juneteenth National Independence Day": [6,19],
+  "Independence Day": [7,4],
+  "Labor Day": [9,[1,1]], // First Monday in September
+  "Columbus Day/Indigenous People's Day": [10,[1,2]], // 2nd Monday in October
+  "Veteran's Day": [11,11],
+  "Thanksgiving Day": [11,[4,4]], // 4th Thursday in November
+  "Christmas Day": [12,25],
+};
+
+const HOLIDAYS_BY_MONTH: { [k: number]: Array<{ def: Def, name: string }> } = {};
+Object.entries(HOLIDAYS).forEach(([name, def]) => {
+  const m = def[0];
+  const d = def[1];
+  if (!HOLIDAYS_BY_MONTH[m]) {
+    HOLIDAYS_BY_MONTH[m] = [];
+  }
+  HOLIDAYS_BY_MONTH[m].push({ def: d, name });
+});
+
+
+// Note that this does not work for dates prior to 2007 since the rules
+// were different.
+const DST_START: [number, [number, number]] = [3,[0,2]]; // 2nd Sunday in March
+const DST_END: [number, [number, number]] = [11,[0,1]]; // 1st Sunday in November
+
+const BUSINESS_HOURS = [9,17]; // In "business-local tz" (Eastern)
+
+// These get the DST start and end for US Eastern TZ
+export function getDSTStart (year: number) {
+  const d = getNthDayOfMonth(year, DST_START[0] - 1, DST_START[1][0], DST_START[1][1]);
+  return new Date(`${ d.toISOString().split('T')[0] }T02:00:00-0400`);
+}
+export function getDSTEnd (year: number) {
+  const d = getNthDayOfMonth(year, DST_END[0] - 1, DST_END[1][0], DST_END[1][1]);
+  return new Date(`${ d.toISOString().split('T')[0] }T02:00:00-0500`);
+}
+
+function datesAreTheSame (a: Date, b: Date) {
+  return (
+    a.getUTCFullYear() === b.getUTCFullYear()
+    && a.getUTCMonth() === b.getUTCMonth()
+    && a.getUTCDate() === b.getUTCDate()
+  );
+}
+
+function getNthDayOfMonth (year: number, month: number, dayOfWeek: number, nth: number) {
+  // Operates at 1200 hours UTC to avoid DST issues for the US
+  let nthAsDate = new Date(year, month, 1, 12, 0, 0, 0);
+  if (nth > 0) { // Relative to start of month
+    // Walk forward until we reach the correct day of the week
+    while (nthAsDate.getUTCDay() !== dayOfWeek) {
+      nthAsDate = new Date(nthAsDate.getTime() + ONE_DAY_MS);
+    }
+    if (nth > 1) {
+      // Walk forward a number of weeks to get the nth
+      nthAsDate = new Date(nthAsDate.getTime() + ONE_DAY_MS * 7 * (nth - 1));
+    }
+  } else { // Relative to end of month
+    // Change start to end of month (go forward 1 month then back 1 day)
+    nthAsDate.setMonth(month + 1); // Will roll over the year if `month` is 11 (December)
+    nthAsDate.setDate(0); // Will roll month back
+    // Walk backward until we reach the correct day of the week
+    while (nthAsDate.getUTCDay() !== dayOfWeek) {
+      nthAsDate = new Date(nthAsDate.getTime() - ONE_DAY_MS);
+    }
+    // Walk backward a number of weeks to get the nth
+    if (nth < -1) {
+      nthAsDate = new Date(nthAsDate.getTime() - ONE_DAY_MS * 7 * (Math.abs(nth) - 1));
+    }
+  }
+  return nthAsDate;
+}
+
+export function checkIfFedBankHoliday (date: Date) {
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const dayOfMonth = date.getDate();
+  const dayOfWeek = date.getDay();
+
+  if (HOLIDAYS_BY_MONTH[month + 1]) {
+    for (let holidayDef of HOLIDAYS_BY_MONTH[month + 1]) {
+
+      if (Array.isArray(holidayDef.def)) {
+        const holidayDefDayOfWeek = holidayDef.def[0];
+        const holidayDefNth = holidayDef.def[1];
+        // Dynamic holiday, the nth day of week in the month (note the ones the
+        // Fed observes are always on weekdays)
+        if (dayOfWeek === holidayDefDayOfWeek) { // Quick check to see if it's worth computing the actual date
+          const holiday = getNthDayOfMonth(year, month, holidayDefDayOfWeek, holidayDefNth);
+          if (datesAreTheSame(holiday, date)) {
+            return holidayDef.name;
+          }
+        }
+
+      } else if (holidayDef.def === dayOfMonth) {
+        // Static holiday during week exact match
+        // (assumes the weekday constraint in nextBankingDay)
+        return holidayDef.name;
+
+      } else {
+        // Static holiday lands on Sunday, observed Monday
+        let holiday = new Date(year, month, holidayDef.def);
+        if (holiday.getDay() === 0) {
+          holiday = new Date(holiday.getTime() + ONE_DAY_MS);
+        }
+        if (datesAreTheSame(holiday, date)) {
+          return holidayDef.name;
+        }
+      }
+    }
+  }
+  return false;
+}
+
+function checkIfWeekday (date: Date) {
+  const dayOfWeek = date.getDay();
+  return dayOfWeek > 0 && dayOfWeek < 6;
+}
+
+export function checkIfBankingDay (date: Date) {
+  let holiday;
+  const isWeekday = checkIfWeekday(date);
+  if (isWeekday) {
+    holiday = checkIfFedBankHoliday(date);
+  }
+  return [
+    isWeekday && !holiday,
+    holiday,
+  ];
+}
+
+function isDSTActive (date: Date) {
+  const localYear = date.getFullYear();
+  const dstStartThisYear = getDSTStartMemoized(localYear);
+  const dstEndThisYear = getDSTEndMemoized(localYear);
+  if (date.getTime() >= dstStartThisYear.getTime()) {
+    if (date.getTime() < dstEndThisYear.getTime()) { // DST end does not include DST
+      // In DST
+      return true;
+    } else {
+      // After DST
+      return false;
+    }
+  } else {
+     // Before DST
+    return false;
+  }
+}
+
+function _pad (n: number) {
+  if (n < 10) {
+    return `0${ n }`;
+  }
+  return n.toString();
+}
+
+// Don't expect to be checking the DST for more than a couple years in a given
+// memory session so this should not be a memory leak.
+const getDSTStartMemoized = memoize(getDSTStart);
+const getDSTEndMemoized = memoize(getDSTEnd);
+
+// Count is the minimum number of business days to advance
+export default function nextBankingDay (date: Date, count=1) {
+
+  const localYear = date.getFullYear(); // Okay if near a year boundary because DST changes are months away
+  const localMonth = date.getMonth();
+  const localDate = date.getDate();
+
+  // Anchor business hours to EST or EDT
+  let businessTZOffset = -5;
+  if (isDSTActive(date)) {
+    businessTZOffset = -4;
+  }
+
+  // Calculate the end of the business hours for that day
+  const businessEnd = new Date(`${ localYear }-${ _pad(localMonth + 1) }-${ _pad(localDate) }T${ BUSINESS_HOURS[1] }:00:00-0${ Math.abs(businessTZOffset) }:00`);
+  let startingMS = date.getTime();
+  let businessEndMS = businessEnd.getTime();
+
+  // If start is after end of business hours that day, move it to the start of
+  // business the next day (+ 16 hours), otherwise move it to start of business
+  // today. (Ignores weekday vs weekend because that will be accounted for
+  // later.) Aligning the reference date to start of business hours avoids
+  // needing to account for its TZ offset later when comparing.
+  if (startingMS > businessEndMS) {
+    startingMS = businessEndMS + ONE_HOUR_MS * 16;
+  } else {
+    startingMS = businessEndMS - ONE_HOUR_MS * 8;
+  }
+
+  let next: Date,
+   upcomingHoliday;
+  let numCalendarDaysToAdvance = 1;
+  let numBankDaysFound = 0;
+  while (numBankDaysFound < count) {
+    // Walk forward one day at a time until an eligible day is found.
+    next = new Date(startingMS + numCalendarDaysToAdvance * ONE_DAY_MS);
+    const [isBankDay, matchedHoliday] = checkIfBankingDay(next);
+    if (isBankDay) {
+      numBankDaysFound += 1;
+    } else if (matchedHoliday) {
+      // Capture the last crossed holiday for feedback to the user.
+      upcomingHoliday = matchedHoliday;
+    }
+    numCalendarDaysToAdvance += 1;
+  }
+
+  // Adjust the resulting date by one hour if it crossed a DST boundary,
+  // either forward or backward depending on which boundary it crossed, so that
+  // it is 9am ET.
+  const nextInDST = isDSTActive(next!);
+  if (businessTZOffset === -5 && nextInDST) {
+    next = new Date(next!.getTime() - ONE_HOUR_MS);
+  } else if (businessTZOffset === -4 && !nextInDST) {
+    next = new Date(next!.getTime() + ONE_HOUR_MS);
+  }
+
+  return [next!, upcomingHoliday] as [Date, string | true | undefined];
+}
