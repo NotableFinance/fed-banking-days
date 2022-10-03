8ca79e497115a7b9aae11845b5718ddf1feb223b
Brendan Kemp <brendan.kemp@gmail.com>
2022-10-03 13:30:42 -0600

    wip

--- src/nextBankingDay.ts
+++ src/nextBankingDay.ts
@@ -201,8 +201,10 @@ const getDSTStartMemoized = memoize(getDSTStart);
 const getDSTEndMemoized = memoize(getDSTEnd);
 
 // Count is the minimum number of business days to advance
-export default function nextBankingDay (date: Date, count=1) {
-
+export default function nextBankingDay (date: Date, count= 1, options: { dayEndsAt5pm?: boolean } = {}) {
+  // If the day ends at 5pm, then nextBankingDay('Thursday 5:15pm') === Monday
+  // It's a more conservative estimate of the next banking day, for estimating deposit availability
+  const { dayEndsAt5pm = true } = options;
   const localYear = date.getFullYear(); // Okay if near a year boundary because DST changes are months away
   const localMonth = date.getMonth();
   const localDate = date.getDate();
@@ -223,10 +225,12 @@ export default function nextBankingDay (date: Date, count=1) {
   // today. (Ignores weekday vs weekend because that will be accounted for
   // later.) Aligning the reference date to start of business hours avoids
   // needing to account for its TZ offset later when comparing.
-  if (startingMS > businessEndMS) {
-    startingMS = businessEndMS + ONE_HOUR_MS * 16;
-  } else {
-    startingMS = businessEndMS - ONE_HOUR_MS * 8;
+  if (dayEndsAt5pm) {
+    if (startingMS > businessEndMS) {
+      startingMS = businessEndMS + ONE_HOUR_MS * 16;
+    } else {
+      startingMS = businessEndMS - ONE_HOUR_MS * 8;
+    }
   }
 
   let next: Date,
